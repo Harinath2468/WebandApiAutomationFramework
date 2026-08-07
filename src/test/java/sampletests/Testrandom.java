aa22e34e8c52452ef477a6b8d87b7d50014344b4
Harinath2468 <Harinathreddyallam@gmail.com>
2026-08-07 19:04:34 +0530

    added some methods

--- src/test/java/sampletests/Testrandom.java
+++ src/test/java/sampletests/Testrandom.java
@@ -4,6 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import utils.GeneralUtils;
 
@@ -13,7 +15,7 @@ import java.time.Duration;
 public class Testrandom {
     WebDriver driver;
 
-    @Test
+    @BeforeClass
     public void launchBrowser(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
@@ -22,21 +24,32 @@ public class Testrandom {
     @Test
     public void login() {
         try {
-            driver.get(GeneralUtils.getProperty("config", "url"));
+            driver.get(GeneralUtils.getConfigFile("config", "url"));
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-            driver.findElement(By.name(GeneralUtils.getProperty("locators", "username"))).sendKeys(GeneralUtils
-                    .getProperty("config", "username"));
-            driver.findElement(By.name(GeneralUtils.getProperty("locators", "password"))).sendKeys(GeneralUtils
-                    .getProperty("config", "password"));
-            driver.findElement(By.xpath(GeneralUtils.getProperty("locators", "login"))).click();
+            driver.findElement(By.name(GeneralUtils.getLocator( "username"))).sendKeys(GeneralUtils
+                    .getConfigFile("config", "username"));
+            driver.findElement(By.name(GeneralUtils.getLocator("password"))).sendKeys(GeneralUtils
+                    .getConfigFile("config", "password"));
+            driver.findElement(By.xpath(GeneralUtils.getLocator("login"))).click();
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
     }
 
     @Test
-    public void teardown() throws InterruptedException {
-        driver.quit();
+    public void aadUser() throws IOException {
+        driver.findElement(By.xpath(GeneralUtils.getLocator("timesheets"))).click();
+//        GeneralUtils.waitForElementTobeVisible(driver,"adminMenu.button").click();
+        driver.findElement(By.xpath(GeneralUtils.getLocator("adminMenu.button"))).click();
+        driver.findElement(By.xpath(GeneralUtils.getLocator("addUser.button"))).click();
+        driver.findElement(By.xpath(GeneralUtils.getLocator( "userRole"))).click();
+        GeneralUtils.hoverAndClick(driver,GeneralUtils.getLocator("ESS"));
+
     }
+
+//    @AfterClass
+//    public void teardown() throws InterruptedException {
+//        driver.quit();
+//    }
 }
